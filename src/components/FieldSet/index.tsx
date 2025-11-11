0dda3477a7209e8182d6d5ca84e571480a6a90df
bielwolf <gabryellobo@gmail.com>
2025-11-11 11:09:20 -0300

    Rederizando lista e renomeação de arquivos

---
+++ src/components/FieldSet/index.tsx
@@ -0,0 +1,18 @@
+import styles from './FieldSet.module.css'
+
+type FieldSetProps = {
+    variant: "primary" | "secondary"
+} & React.FieldsetHTMLAttributes<HTMLFieldSetElement>;
+
+const FieldSet = ({ children, variant = "primary" }: FieldSetProps) => {
+  return (
+    <fieldset className={`${styles.inputWrapper} ${
+        variant === "primary" ? styles.primary : styles.secondary
+    }`}
+        >
+        {children}
+    </fieldset>
+  )
+}
+
+export default FieldSet
